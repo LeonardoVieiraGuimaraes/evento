c1434e4a1c01ee9d68a68d775b208ebd77902a85
LEONARDO VIEIRA GUIMARAES <42416757+LeonardoVieiraGuimaraes@users.noreply.github.com>
2022-11-09 13:23:54 -0300

    update index favicon

--- src/main/java/com/evento/evento/controllers/IndexController.java
+++ src/main/java/com/evento/evento/controllers/IndexController.java
@@ -1,13 +1,14 @@
 package com.evento.evento.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-
+@RequestMapping("/")
 public class IndexController {
-    @RequestMapping("/")
+    @GetMapping
     public String indexString() {
-        return "eventos/listaEventos";
+        return "redirect:/eventos";
     }
 }
