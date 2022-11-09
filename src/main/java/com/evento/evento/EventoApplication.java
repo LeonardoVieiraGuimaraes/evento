9fbec554b93cacb5bd69e0f642d7783592b8ac4d
LEONARDO VIEIRA GUIMARAES <42416757+LeonardoVieiraGuimaraes@users.noreply.github.com>
2022-11-09 17:05:14 -0300

    te

--- src/main/java/com/evento/evento/EventoApplication.java
+++ src/main/java/com/evento/evento/EventoApplication.java
@@ -2,7 +2,9 @@ package com.evento.evento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan
 @SpringBootApplication(scanBasePackages = "com.evento.evento.controllers")
 public class EventoApplication {
 
